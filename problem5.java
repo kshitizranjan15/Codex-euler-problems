58cf215ca24b69089a7b7dffd01a5bdcb988d8c2
KSHITIZ RANJAN <54390873+kshitizranjan15@users.noreply.github.com>
2020-06-20 07:09:09 +0530

    Update problem5.java

--- problem5.java
+++ problem5.java
@@ -1,22 +1,43 @@
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-public class problem5 
+public class problem5
 {
-	public static void main(String[] args)
-	{
-		int i,n,a;
-		n=1000000000;
-		for(i=2;i<n;i++)
-		{
-			if(i%2==0 && i%3==0 && i%4==0 && i%5==0 && i%6==0 && i%7==0 && i%8==0 && i%9==0 && i%10==0 && i%11==0 && i%12==0 && i%13==0 && i%14==0 && i%15==0 && i%16==0 && i%17==0 && i%18==0 && i%19==0 && i%20==0)
-			{
-				System.out.println(i);
-				break;
-			}
-			
-		}
-		
-		
-	}
+	public static void main(String[] arg) { 
+	      int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
+	      System.out.println(lcm(arr)); 	} 
+
+		public static long lcm(int[] array){ 
+		        long lcm = 1; 
+		        int r = 2; 
+		          
+		        while (true) { 
+		            int counter = 0; 
+		            boolean loop = false; 
+		              
+		            for (int i = 0; i < array.length; i++) { 
+		  
+		                if (array[i] == 0) { 
+		                    return 0; 
+		                } 
+		               
+		                if (array[i] == 1) { 
+		                    counter++; 
+		                }  
+		                if (array[i] % r == 0) { 
+		                    loop = true; 
+		                    array[i] = array[i] / r; 
+		                } 
+		            } 
+		            if (loop) { 
+		                lcm = lcm * r; 
+		            } 
+		            else { 
+		                r++; 
+		            }  
+		            if (counter == array.length) { 
+		                return lcm; 
+		            } 
+		        } 
+		    } 
 
 }
 //output:-232792560
